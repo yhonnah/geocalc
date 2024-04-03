c568ad5a43a43a26dc140e620a9ad80e5d48cc66
yhonnah <163302710+yhonnah@users.noreply.github.com>
2024-04-03 20:52:21 +0800

    started the formula functions

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # main code for calculator (commit nyo sa vscode para maka push and pull tayo ng code ng madali)
+
+from math import pi
+
+def sphere_radius(r):
+    area = 4 * pi * (r ** 2)
+    volume = (4/3) * pi * (r ** 3)
+
+    return area, volume
+
